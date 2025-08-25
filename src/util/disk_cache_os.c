bminor/mesa-mesa

commit ff9ba2cef0d97fdf5aa44680c874924b3bd663e4

util/disk_cache: Fallback to ftruncate if posix_fallocate not supported
The posix_fallocate itself may be present and callable but filesystem
may not support it. Happens at least on Android.

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36821>

src/util/disk_cache_os.c
@@ -1126,8 +1126,15 @@ disk_cache_mmap_cache_index(void *mem_ctx, struct disk_cache *cache)
       /* posix_fallocate() ensures disk space is allocated otherwise it
        * fails if there is not enough space on the disk.
        */
-      if (posix_fallocate(fd, 0, size) != 0)
-         goto path_fail;
+      int ret = posix_fallocate(fd, 0, size);
+      if (ret != 0) {
+         if (ret == EOPNOTSUPP) {
+            if (ftruncate(fd, size) == -1)
+               goto path_fail;
+         } else {
+            goto path_fail;
+         }
+      }
 #else
       /* ftruncate() allocates disk space lazily. If the disk is full
        * and it is unable to allocate disk space when accessed via

Devsh-Graphics-Programming/mesa

commit 0b665c0ca37689442c0505a7f989092b335fb583

util/disk_cache: fall back to ftruncate on FreeBSD
posix_fallocate with ZFS on FreeBSD < 15 returns EINVAL unlike Linux.

https://cgit.freebsd.org/src/commit/?id=96ed2690dfb3
https://cgit.freebsd.org/src/commit/?id=478b09577a47

Fixes: ff9ba2cef0d9 ("util/disk_cache: Fallback to ftruncate if posix_fallocate not supported")
Reviewed-by: Juston Li <justonli@google.com>

src/util/disk_cache_os.c
@@ -1131,7 +1131,7 @@ disk_cache_mmap_cache_index(void *mem_ctx, struct disk_cache *cache)
        */
       int ret = posix_fallocate(fd, 0, size);
       if (ret != 0) {
-         if (ret == EOPNOTSUPP) {
+         if (ret == EOPNOTSUPP || ret == EINVAL) {
             if (ftruncate(fd, size) == -1)
                goto path_fail;
          } else {